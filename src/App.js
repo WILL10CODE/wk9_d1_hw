f18cebfbaf72a645edc53d1f07c98df78f861ae3
WILL10CODE <wblumenthal100@gmail.com>
2022-01-19 20:52:35 -0600

    Will's Hw

--- src/App.js
+++ src/App.js
@@ -1,51 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Window from './Window';
-import ToDo from './ToDo';
-
-export default function App() {
-
-  const fib = () => {
-    console.log('Added')
-    return 0
-  }
-
-  let [count, setcount] = useState(() => fib());
-
-  const addCount = () => {
-    setcount(prevCount => prevCount + 1)
-  }
-
-  const {pokemon, setPokemon} = useState({})
-
-  const getData = async () => {
-    const res = await fetch(" https://pokeapi.co/api/v2/pokemon/ditto");
-    const data = await res.json()
-    console.log("Got the Pokemon")
-    return data
-  }
-
-  useEffect(async ()=>{
-    const data = await getData();
-    setPokemon(data)
-
-  }, [])
+import React from 'react';
+import './App.css';
 
 
-    return (
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
       <div>
-        <h1>"Will's Functional Component"</h1>
-        <h3>Count Me: {count}</h3>
-        <button onClick={() =>{addCount()}}>+</button>
-
-        <Routes>
-          <Route path ='/' element={<Home />}/>
-          <Route path ='/window' element={<Window />}/>
-          <Route path ='/todo' element={<ToDo />}/>
-        </Routes>
-        
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>x</button>
       </div>
-    )
+    </div>
+  );
+};
+
+function TodoForm({ addToDo, index }) {
+  const [value, setValue] = React.useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addToDo(addToDo);
+    setValue("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <button onClick={() => addToDo(index)}>Add To List</button>
+    </form>
+  );
 }
+function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Eat Breakfast", isCompleted: false },
+    { text: "Go to the Gym", isCompleted: false },
+    { text: "Watch South Park", isCompleted: false },
+    { text: "Code on Computer", isCompleted: false },
+    { text: "Eat Dinner", isCompleted: false },
+    { text: "Sleep", isCompleted: false },
+
+  ]);
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+  return (
+    <div className="app">
+      <div className="to-do-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+export default App;
+
+
 
